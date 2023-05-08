168ede2eb05fda4fd091fe0879793c9f7cb27a82
Hansen <bhansen3@ad.brown.edu>
2023-05-08 14:30:54 -0400

    Update preprocessing and analyze shoemaker data

--- src/plotting/latent_pca.py
+++ src/plotting/latent_pca.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from scipy import stats
 
+import numpy as np
+
 
 def plot_cluster_pca(X, y, ax=None, **plt_kwargs):
     if ax is None:
@@ -12,4 +14,19 @@ def plot_cluster_pca(X, y, ax=None, **plt_kwargs):
     ax.scatter(x_new[:, 0], x_new[:, 1], c=y, **plt_kwargs)
     ax.set_xlabel("PC1")
     ax.set_ylabel("PC2")
+    return ax
+
+def plot_cluster_pca_real_data(X, group_int, group_text, ax=None, **plt_kwargs):
+    if ax is None:
+        ax = plt.gca()
+    X = stats.zscore(X)
+    pca = PCA()
+    x_new = pca.fit_transform(X)
+    ax.scatter(x_new[:, 0], x_new[:, 1], c=group_int, label=np.unique(group_text), **plt_kwargs)
+    ax.set_xlabel("PC1")
+    ax.set_ylabel("PC2")
+    
+    ax.legend()
+    
+    
     return ax
