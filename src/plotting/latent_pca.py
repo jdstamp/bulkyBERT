5f61c29c4277020f6e9558d1efc18af0ceb0a3fe
Hansen <bhansen3@ad.brown.edu>
2023-05-08 14:37:13 -0400

    undo changes to plotting

--- src/plotting/latent_pca.py
+++ src/plotting/latent_pca.py
@@ -4,8 +4,6 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from scipy import stats
 
-import numpy as np
-
 
 def plot_cluster_pca(X, y, ax=None, **plt_kwargs):
     if ax is None:
@@ -18,19 +16,4 @@ def plot_cluster_pca(X, y, ax=None, **plt_kwargs):
         ax.scatter(x_new[ix, 0], x_new[ix, 1], label=g)
     ax.set_xlabel("PC1")
     ax.set_ylabel("PC2")
-    return ax
-
-def plot_cluster_pca_real_data(X, group_int, group_text, ax=None, **plt_kwargs):
-    if ax is None:
-        ax = plt.gca()
-    X = stats.zscore(X)
-    pca = PCA()
-    x_new = pca.fit_transform(X)
-    ax.scatter(x_new[:, 0], x_new[:, 1], c=group_int, label=np.unique(group_text), **plt_kwargs)
-    ax.set_xlabel("PC1")
-    ax.set_ylabel("PC2")
-    
-    ax.legend()
-    
-    
     return ax
